a200f6f30c3fe1cb987b602f74b4a9ec63267cbc
TDGTom <78016707+TDGTom@users.noreply.github.com>
2022-09-21 11:22:24 +0200

    login

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 <meta charset="UTF-8">
-<title>Login Page with Username and Password Example</title>
+<title>Login Page with Usernahe and Password Example</title>
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet"><link rel="stylesheet" href="./style.css">
 </head>
 <body>
