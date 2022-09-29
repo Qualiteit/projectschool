e2e581bb96ec241ebb71d894f4514ca3c0cee194
aa254365 <113343284+aa254365@users.noreply.github.com>
2022-09-29 09:41:43 +0200

    Update tello.py

--- tello.py
+++ tello.py
@@ -11,3 +11,17 @@ class Tello(object):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.bind((self.local_ip, self.local_port))
         
+        self.receive_thread = threading.Thread(target=self._receive_thread)
+        self.receive_thread.daemon = True
+        self.receive_thread.start()
+        
+        self.tello_ip = ''
+        self.tello_port = 8889
+        self.tello_address = (self.tello_ip, self.tello_port)
+        self.log = []
+
+        self.MAX_TIME_OUT = 15.0
+
+        def send_command(self, command):
+
+
