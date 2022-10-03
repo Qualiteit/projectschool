5b6a7c6aad5a26d760433343f76eb6735c003b24
aa254365 <113343284+aa254365@users.noreply.github.com>
2022-10-03 09:34:49 +0200

    object tracking

--- ObjectTrackingTello.py
+++ ObjectTrackingTello.py
@@ -5,3 +5,19 @@ import numpy as np
 width = 640
 height = 480
 deadZone = 100
+
+startCounter = 0
+
+me = Tello()
+me.connect()
+me.for_back_velocity = 0
+me.left_right_velocity = 0
+me.up_down_velocity = 0
+me.yaw_velocity = 0
+me.speed = 0
+
+print(me.get_battery())
+
+me.streamoff()
+
+me.streamon()
