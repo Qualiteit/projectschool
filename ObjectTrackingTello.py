2ab7bddb7f1de89aae307992d94e9c6d31a973b0
aa254365 <113343284+aa254365@users.noreply.github.com>
2022-10-05 09:58:00 +0200

    update object tracking

--- ObjectTrackingTello.py
+++ ObjectTrackingTello.py
@@ -54,3 +54,67 @@ for x in range (0, rows):
             imgArray[x][y] = cv2.resize(imgArray[x][y], (0,0), None, scale, scale)
             else:
                 imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+                imageBlank = np.zeros((height, width, 3), np.uint8)
+                hor = [imageBlank]*rows
+                hor_con = [imageBlank]*rows
+                for x in range(0, rows):
+                    hor[x] = np.hstack(imgArray[x])
+
+                    ver = np.vstack(hor)
+                    else:
+                        for x in range(0, rows):
+                            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+                                else:
+                                    imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None, scale, scale)
+                                    if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+                                    hor = np.hstack(imgArray)
+                                    ver = hor
+                                    return ver
+
+
+                                    def getContours(img, imgContour):
+                                        global dir
+
+                                        contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+                                        for cnt in contours:
+                                            area = cv2.contourArea(cnt)
+                                            areaMin = cv2.getTrackbarPos("Area", "Parameters")
+                                            if area > areaMin:
+                                                cv2.drawContours(imgContour, cnt, -1(255, 0, 255), 7)
+                                                peri = cv2.arcLength(cnt, True)
+                                                approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
+
+
+
+
+                                                x , y , w , h = cv2.boundingRect(approx)
+                                                cx = int(x + (w / 2))
+                                                cy = int(y +(h / 2))
+
+                                                if (cx <int(frameWidth/2)-deadZone):
+                                                    cv2.putText(imgContour, "Go left", (20, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 3)
+                                                    cv2.rectangle(imgContour,(0, int(frameHeight/2-deadZone)),(int(frameWidth/2)-deadZone, int(frameHeight/2)+deadZone),(0,0,255), cv2.FILLED)
+                                                    dir = 1
+
+                                                    elif (cx > int(frameWidth / 2) + deadZone):
+                                                    cv2.putText(imgContour, "Go right", (20, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 3)
+                                                    cv2.rectangle(imgContour,(int(frameWidth/2+deadZone),int(frameHeight/2-deadZone)),(frameWidth,int(frameHeight/2)+deadZone),(0,0,255), cv2.FILLED)
+                                                    dir = 2
+
+                                                    elif  (cy < int(frameHeight / 2)- deadZone):
+                                                        cv2.putText(imgContour, "Go up", (20, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 3)
+                                                        cv2.rectangle(imgContour,(int(frameWidth/2-deadZone), int(frameHeight/2)+deadZone),(int(frameWidth/2+deadZone),frameHeight),(0,0,255),cv2.FILLED)
+                                                        dir = 3
+                                                      
+                                                      elif (cy > int(frameHeight / 2) + deadZone):
+                                                        cv2.putText(imgContour, "Go down", (20, 50) cv2.FONT_HERSHEY_COMPLEX, 1,(0,0,255), 3)
+                                                        cv2.rectangle(imgContour,(int(frameWidth/2-deadZone), int(frameHeight/2)+deadZone),(int(frameWidth/2+deadZone),frameHeight),(0,0,255),cv2.FILLED)
+                                                        else: dir=0
+                                                        
+
+
+
+
+
