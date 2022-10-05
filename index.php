cbe97d3a21d717347ca273398ec6e8eb2faea268
Qualiteit <51707651+Qualiteit@users.noreply.github.com>
2022-10-05 11:34:42 +0200

    buttons

--- index.php
+++ index.php
@@ -13,11 +13,22 @@ include 'FaceTrackingTello.py';
 <body>
     <div class="display-drone">
         <div class="drone-cam">
-            <p>hi</p>
+        <canvas id="video-canvas"></canvas>
+	        <script type="text/javascript" src="jsmpeg.min.js"></script>
+	            <script type="text/javascript">
+	          	    var canvas = document.getElementById('video-canvas');
+		            var url = 'ws://'+document.location.hostname;
+		            var player = new JSMpeg.Player(url, {canvas: canvas});
+	            </script>
         </div>
         <div class="drone-actions">
             <p>Drone battery:</p> 
             <? echo shell_exec("python battery.py"); ?>
+        <button class="connect-button button-general">Connect</button>
+        <button class="takeoff-button button-general">Takeoff</button>
+        <button class="land-button button-general">Land</button>
+        <button class="handcontrol-button button-general">Hand controls</button>
+        <button class="facetracking-button button-general">Face tracking</button>
         </div>
     </div>
 </body>
