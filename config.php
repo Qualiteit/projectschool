372622387dd4c081d4499c1f59b356ba0580a860
NoahNaki <74353773+NoahNaki@users.noreply.github.com>
2022-10-05 10:25:20 +0200

    adminuser

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', '40.118.46.53');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'mysql1234');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'password');
 define('DB_NAME', 'drone');
  
 /* Attempt to connect to MySQL database */
