77313325f475d5bd6f4e07bfa7a6f2cc7f70970a
aa254365 <113343284+aa254365@users.noreply.github.com>
2022-10-06 09:24:20 +0200

    removed registration link

--- login.php
+++ login.php
@@ -124,7 +124,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             <div class="form-group">
                 <input type="submit" class="btn btn-primary" value="Login">
             </div>
-            <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
         </form>
     </div>
 </body>
