44c2fbb6758b4817aded069157604697520703e6
Qualiteit <51707651+Qualiteit@users.noreply.github.com>
2022-10-10 10:54:17 +0200

    summary

--- app.py
+++ app.py
@@ -1,45 +1,20 @@
-from tello import tello
-import sys
-from datetime import datetime
-import time
-import argparse
+from flask import Flask, render_template, Response, request, redirect, url_for
+from djitellopy import Tello
+import cv2
+import numpy as np
+app = Flask(__name__)
 
-def parse_args(args):
+@app.route("/")
+def index():
+    return render_template('index.html')
 
-    parser = argprarse.ArgumentParser('Tello Flight Commander',
-    epilog='One-Off Coder https://www.oneoffcoder.com')
-
-    parser.add_argument('-f', '--file', help='command file', required=True)
-    return parser.parse_args(args)
-
-    def start(file_name): 
-
-        start_time = str(datetime.now())
-
-        with open(file_name, 'r') as f:
-            commands = f.readlines()
-
-            tello = Tello()
-            for command in commands:
-                if command != '' and command != '\n':
-                    command = command.rstrip()
-
-                    if command.find('delay') != -1:
-                        sec = float(command.partition('delay')[2])
-                        print(f'delay {sec}')
-                        time.sleep()
-                        pass
-                    else:
-                        tello.send_command(command)
-
-                        with open(f'log/{start_time}.txt', 'w') as out:
-                            log = tello.get_log()
-
-                            for stat in log:
-                                stat.print_stats()
-                                out.write(s)
-
-                                if __name__ == '__main__':
-                                    args = parse_args(sys.args[1:])
-                                    file_name = args.file
-                                    start(file_name)
+@app.route('/takeoff', methods=['GET', 'POST'])
+def connect():
+    print('Drone is taking off')
+    try:
+        myDrone.connect()
+        print('The drone is connected!')
+        myDrone.takeoff()
+    except:
+        print('something went wrong')
+    return render_template("index.html")
